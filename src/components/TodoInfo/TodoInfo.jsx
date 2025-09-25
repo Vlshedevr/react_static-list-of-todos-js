ee0dd06ff741c2d6fd02ff97a15f634ef32fa2dc
Volodymyr Kolosov <kolosovvova0@gmail.com>
2025-09-25 13:34:24 +0300

    task list V 1.0.1

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -3,7 +3,9 @@ import { UserInfo } from '../UserInfo/UserInfo';
 // Add the required props
 export const TodoInfo = ({ todo }) => {
   return (
-    <article className={`TodoInfo ${todo.completed && 'TodoInfo--completed'}`}>
+    <article
+      className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
+    >
       <h2 className="TodoInfo__title">{todo.title}</h2>
 
       {todo.user && <UserInfo user={todo.user} />}
